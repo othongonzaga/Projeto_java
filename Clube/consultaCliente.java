e03bf13bcd62d644730b003b9f60ffa981526de0
othongonzaga <othoncesar1@hotmail.com>
2022-04-19 12:06:39 -0300

    Criando classe colecaoCliente

---
+++ Clube/consultaCliente.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ColecaoClientes{
+    ArrayList<cliente> meusClientes = new ArrayList<cliente>();
+
+    public void adicionar(cliente c){
+        meusClientes.add(c);
+    }
+
+    public void remover(cliente c){
+        
+    }
+
+    public void procurar(String nome){
+    
+    }
+}
